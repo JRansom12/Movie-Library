3086ddb475905ecf49b73c7de5f24f64c71e8626
Jon Ransom <jransom12@live.com>
2019-09-12 10:49:06 -0500

    GET API call
    Built GET API call in app.js using XMLHttpRequest, addEventListener, renderHTML etc. Working on table to display movieList array when browser is opened or refreshed.

--- Client/app.js
+++ Client/app.js
@@ -2,13 +2,14 @@
     function processForm( e ){
         var dict = {
         	Title : this["title"].value,
+          Genre: this["genre"].value,
         	Director: this["director"].value
         };
 
         $.ajax({
             url: 'https://localhost:44352/api/movie',
             dataType: 'json',
-            type: 'post',
+            type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
@@ -24,3 +25,45 @@
 
     $('#my-form').submit( processForm );
 })(jQuery);
+
+
+var movieContainer = document.getElementById("movieInfo")
+var movieList = [];
+document.getElementById("btn").addEventListener("click", function(){
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://localhost:44352/api/movie')
+  ourRequest.onload = function functionName() {
+    var movieList = JSON.parse(ourRequest.responseText);
+    renderHTML(movieList);
+  }
+  ourRequest.send();
+});
+
+function renderHTML(data){
+  var movieString = "";
+  for(i = 0; i < data.length; i++){
+    movieString += "<p>" + data[i].Title + " " + data[i].Genre + " " + data[i].Director + "</p>";
+  }
+  movieContainer.insertAdjacentHTML('beforeend', movieString);
+}
+
+window.addEventListener("load", function(){
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://localhost:44352/api/movie')
+  ourRequest.onload = function functionName() {
+    var movieList = JSON.parse(ourRequest.responseText);
+    renderHTML(movieList);
+  }
+  ourRequest.send();
+var perrow = 3,
+    html = "<table><tr>";
+for (var i=0; i<movieList.length; i++) {
+  html += "<td>" + "movieList[i].Title" + "</td>";
+  var next = i+1;
+  if (next%perrow==0 && next!=data.length) {
+    html += "</tr><tr>";
+  }
+}
+html += "</tr></table>";
+document.getElementById("container").innerHTML = html;
+});

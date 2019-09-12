c52e8847e17640a06498a638a8c52cc125a6eb61
Jon Ransom <jransom12@live.com>
2019-09-12 15:56:31 -0500

    GET API call
    GET gets the db.Movie objects and displays them on the screen with document.ready

--- Client/app.js
+++ Client/app.js
@@ -1,11 +1,10 @@
 (function($){
-    function processForm( e ){
+    function processPost( e ){
         var dict = {
         	Title : this["title"].value,
         	Genre: this["genre"].value,
           Director: this["director"].value
         };
-
         $.ajax({
             url: 'https://localhost:44352/api/movie',
             dataType: 'json',
@@ -14,31 +13,31 @@
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
                 $('#response pre').html( data );
+                GetMovies();
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
         });
-
         e.preventDefault();
     }
-
-    $('#my-form').submit( processForm );
+    $('#my-form').submit( processPost );
 })(jQuery);
 
 
-var movieContainer = document.getElementById("movieInfo")
-var movieList = [];
-document.getElementById("btn").addEventListener("click", function(){
+
+var movieContainer = document.getElementById("movieInfo");
+
+$(document).ready(GetMovies());
+function GetMovies(){
   var ourRequest = new XMLHttpRequest();
   ourRequest.open('GET', 'https://localhost:44352/api/movie')
   ourRequest.onload = function functionName() {
     var movieList = JSON.parse(ourRequest.responseText);
     renderHTML(movieList);
+    console.log(movieList);
   }
-  ourRequest.send();
-});
-
+    ourRequest.send();
 function renderHTML(data){
   var movieString = "";
   for(i = 0; i < data.length; i++){
@@ -46,89 +45,4 @@ function renderHTML(data){
   }
   movieContainer.insertAdjacentHTML('beforeend', movieString);
 }
-
-window.addEventListener("load", function(){
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://localhost:44352/api/movie')
-  ourRequest.onload = function functionName() {
-    var movieList = JSON.parse(ourRequest.responseText);
-    renderHTML(movieList);
-  }
-  ourRequest.send();
-var perrow = 3,
-    html = "<table><tr>";
-for (var i=0; i<movieList.length; i++) {
-  html += "<td>" + "movieList[i].Title" + "</td>";
-  var next = i+1;
-  if (next%perrow==0 && next!=data.length) {
-    html += "</tr><tr>";
-  }
 }
-html += "</tr></table>";
-document.getElementById("container").innerHTML = html;
-
-function gettingMovies(){
-  $.ajax({
-    url: "https://localhost:44352/api/movie",
-    type: "get",
-    dataType: "json",
-    async: true,
-    data: JSON,
-    success: function(movies){
-      movieListSuccess(movie);
-    },
-    error: function(request, message, error){
-      handleException(request, message, error);
-    }
-  });
-}
-
-
-
-function addMovie(movies){
-  $.ajax({
-    url: "/api/Movie",
-    type: "post",
-    contentType: "JSON",
-    data: JSON.stringify(product),
-    success: function(movie){
-      movieAddSuccess(movie);
-    },
-  });
-}
-
-
-
-function updateMovie(movies) {
-  $.ajax({
-    url: "/api/Movie",
-    type: 'PUT',
-    contentType:
-       "application/json;charset=utf-8",
-    data: JSON.stringify(movie),
-    success: function (movie) {
-      productUpdateSuccess(movie);
-    },
-    error: function (request, message, error) {
-      handleException(request, message, error);
-    }
-  });
-}
-
-
-
-function deleteMovie(ctl) {
-  var id = $(ctl).data("id");
-
-  $.ajax({
-    url: "/api/Movie" + id,
-    type: 'DELETE',
-    success: function (movie) {
-      $(ctl).parents("tr").remove();
-    },
-    error: function (request, message, error) {
-      handleException(request, message, error);
-    }
-  });
-}
-});

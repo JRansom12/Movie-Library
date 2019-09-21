8d64e50128fbc828dbb7004df4520e671fa04959
Jon Ransom <jransom12@live.com>
2019-09-21 00:20:57 -0500

    GetMovies method added to the bottom of the Post method to refresh the data table without refreshing the browser. Table rows are now links that send the rowData to the Edit form

--- Client/app.js
+++ Client/app.js
@@ -21,6 +21,7 @@
         });
 
         e.preventDefault();
+        GetMovies();
     }
     $('#my-form').submit( processForm );
 })(jQuery);
@@ -33,14 +34,28 @@ function GetMovies(){
     var movieList = JSON.parse(ourRequest.responseText);
       $("#movieTable").find("tr:gt(0)").remove();
     for(i = 0; i < movieList.length; i++){
-      $("#movieTable").append("<tr><td>" + movieList[i].Title + "</td><td>" + movieList[i].Genre + "</td><td>" + movieList[i].Director  + "</td></tr>");
+      $("#movieTable").append("<tr><td>"+ movieList[i].Title + "</td><td>" + movieList[i].Genre + "</td><td>" + movieList[i].Director  + "</td></tr>");
     }
     console.log(movieList);
   }
     ourRequest.send();
   }
 
+$( document ).ready(function() {
+  $("#movieTable tbody").on('click', 'tr', function() {
+      var rowData = $(this).children("td").map(function() {
+          return $(this).text();
+      }).get();
+      $("#titleEdit").val(rowData[0])
+      $("#genreEdit").val(rowData[1])
+      $("#directorEdit").val(rowData[2]);
+  });
+});
+
+
+
 function GetMovieToEdit(){
+  alert("Movies yeah yeah");
   var movieToEdit = movies.filter(function(el){
     if (person.id == el.movies){
       return true;
@@ -50,3 +65,30 @@ function GetMovieToEdit(){
   });
   return movies[i];
 }
+
+(function($){
+    function processForm( e ){
+        var dict = {
+        	Title : this["title"].value,
+        	Genre: this["genre"].value,
+          Director: this["director"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44352/api/movie',
+            dataType: 'json',
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+    $('#my-form').submit( processForm );
+})(jQuery);
